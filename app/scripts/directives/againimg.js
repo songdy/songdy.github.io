892074add9c853de2a0146637e6612cda9426dd8
51b <5.1.b@163.com>
2015-08-15 23:15:13 +0800

    fixes bug for againimg

--- app/scripts/directives/againimg.js
+++ app/scripts/directives/againimg.js
@@ -1,13 +1,13 @@
 'use strict';
 
 app.directive('againimg', function(globalConfig) {
-  var template = '<img  ng-src="' + globalConfig.host + '/again/{{src}}" />';
+  var template = '<img  ng-src="' + globalConfig.host + '/again/{{url}}" />';
   return {
     restrict: 'E',
     template: template,
     replace: true,
     scope: {
-      src: '=src'
+      url: '=url'
     }
   };
 });
