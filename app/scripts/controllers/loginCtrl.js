53d2f5a171ec32afeb5eade0404543827fbba1e3
51b <5.1.b@163.com>
2015-08-19 09:23:26 +0800

    updated

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -5,6 +5,7 @@ app.config(function ($stateProvider) {
     url: '/login',
     controller: function ($location, $state, $http, globalConfig) {
       if ($location.$$search.code) {
+        console.log(!$location.$$search.state);
         $http({
           method: 'POST',
           url: globalConfig.host + '/again/weixin/getUserInfo.do',
@@ -15,13 +16,15 @@ app.config(function ($stateProvider) {
         }).success(function(data) {
           if (!$location.$$search.state) {
             $state.go('main');
+          } else {
+            $location.path(!$location.$$search.state);
           }
           console.log(data);
         }).error(function(err) {
           console.log(err);
         });
       } else {
-        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx87de30e37dd369b6&redirect_uri=http://again.51b.org/login&response_type=code&scope=snsapi_userinfo#wechat_redirect';
+        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx87de30e37dd369b6&redirect_uri=http%3A%2F%2Fagain.51b.org%2Flogin&response_type=code&scope=snsapi_userinfo&state=main#wechat_redirect';
       }
     }
   });
