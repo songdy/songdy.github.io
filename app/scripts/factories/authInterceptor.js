64ca28447aebc9db1dac979a36e95e0c5d548815
51b <5.1.b@163.com>
2015-08-20 11:47:16 +0800

    updated

--- app/scripts/factories/authInterceptor.js
+++ app/scripts/factories/authInterceptor.js
@@ -1,19 +1,18 @@
 'use strict';
 
-app.factory('authInterceptor', function($q, $rootScope, $injector, $location, loading) {
+app.factory('authInterceptor', function($q, $rootScope, $injector, loading) {
 
   var interceptor = {
     request: function(config) {
       loading.show();
       var $state = $injector.get('$state');
+      var $location = $injector.get('$location');
       var deferred = $q.defer();
       if (!$state.is('login')) {
         if (!!localStorage.getItem('accessToken')) {
           config.headers.accessToken = localStorage.getItem('accessToken');
           deferred.resolve(config);
         } else {
-          alert(JSON.stringify($state.current));
-          alert(JSON.stringify($state.get()));
           deferred.reject('accessToken is required');
           $state.go('login', {
             from: $location.absUrl()
