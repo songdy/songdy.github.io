4bff52d85505cd53a0963b8e8d04a67805d5a3a7
51b <5.1.b@163.com>
2015-08-24 00:02:55 +0800

    updated

--- app/scripts/directives/againgoback.js
+++ app/scripts/directives/againgoback.js
@@ -2,9 +2,10 @@
 
 app.directive('againgoback', function() {
   var isIOS = navigator.userAgent.toLowerCase().match(/iphone|ipad/i) || [];
-  var template = isIOS.length > 0 ? '' : '<div class="navbar" ng-hide="">' +
+  var template = isIOS.length > 0 ? '' : '<div class="navbar">' +
     '<a ui-sref="main" class="goback"></a>' +
     '</div>';
+  console.log(template);
   return {
     restrict: 'E',
     template: template,
