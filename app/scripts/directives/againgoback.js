497507a30d0f52c4ffa4fc797456f62f70e5d746
51b <5.1.b@163.com>
2015-08-23 22:24:50 +0800

    updated

--- app/scripts/directives/againgoback.js
+++ app/scripts/directives/againgoback.js
@@ -3,7 +3,7 @@
 app.directive('againgoback', function() {
   var isIOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
   var template = isIOS ? '' : '<div class="navbar" ng-hide="">' +
-    '<a ui-sref="main"><img src="/images/go-back.png" alt="" height="30px" /></a>' +
+    '<a ui-sref="main" class="goback"></a>' +
     '</div>';
   return {
     restrict: 'E',
